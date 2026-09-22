51e970967264ad563ac332f123b3a204ebf94766
Daven Muntande <224035339@nust.na>
2026-09-22 11:58:21 +0200

    Week 2 - Municipal Budget Calculator

---
+++ WEEK1/WEEK2/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    double revenue, expenses, balance;
+
+    printf("MUNICIPAL BUDGET CALCULATOR\n\n");
+
+    printf("Enter Total Revenue: ");
+    scanf("%lf", &revenue);
+
+    printf("Enter Total Expenses: ");
+    scanf("%lf", &expenses);
+
+    balance = revenue - expenses;
+
+    printf("\n---------------------------------\n");
+    printf("Revenue  : %.2f\n", revenue);
+    printf("Expenses : %.2f\n", expenses);
+    printf("Balance  : %.2f\n", balance);
+    printf("---------------------------------\n");
+
+    return 0;
+}
